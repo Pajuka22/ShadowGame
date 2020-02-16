3361c852735de675704c2ef4ae9aef46748c3845
Pajuka22 <nebula374@gmail.com>
2020-02-15 21:13:57 -0800

    creating the Lost Ones

---
+++ enc_temp_folder/743d3523253fa7e462e69376785d64fc/LostOneAI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "LostOneAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWGAME_API ALostOneAI : public AAIController
+{
+	GENERATED_BODY()
+	
+};
